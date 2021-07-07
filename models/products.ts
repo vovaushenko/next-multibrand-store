2fbbfff1292e69397c56e4351923198b5e386d3b
vovaushenko <vovaushenko1989@gmail.com>
2021-07-07 15:49:08 +0300

    Add initial scaffolding of mongoose product model

---
+++ models/products.ts
@@ -0,0 +1,59 @@
+import mongoose from 'mongoose'
+
+const productsSchema = new mongoose.Schema({
+  department: {
+    required: [true, 'Please add product department (men, women, kid)'],
+    type: String,
+    enum: ['men', 'women', 'kid'],
+    default: 'men',
+  },
+  brand: {
+    type: String,
+    required: [true, 'Please add brand name'],
+    maxLength: [15, 'Brand name should not exceed 50 characters'],
+    trim: true,
+  },
+  model: {
+    type: String,
+    required: [true, 'Please add product model'],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Please add product price'],
+  },
+  styleCode: {
+    type: String,
+    required: [true, 'Please add product style code'],
+    trim: true,
+  },
+  colors: [
+    {
+      type: String,
+    },
+  ],
+  size: {
+    type: Number,
+    required: [true, 'Please add product size'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add product description'],
+    trim: true,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: [true],
+      },
+      url: {
+        type: String,
+        required: [true],
+      },
+    },
+  ],
+})
+
+export default mongoose.models.Products ||
+  mongoose.model('Products', productsSchema)
