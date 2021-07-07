b5399955f1e4bb047f1b354201dcd3d9a634ade0
vovaushenko <vovaushenko1989@gmail.com>
2021-07-07 15:29:15 +0300

    Add reusable error handler in the form of a higher-order function

---
+++ middleware/catchErrorsFrom.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+/**
+ * Custom Error Handling Middleware for catching errors in api route controllers
+ * @function
+ * @import this function in any controller and wrap it
+ * @returns error catching boundary for route controllers
+ * @shares the error message with the client by sending it as the body of the 500 response
+ */
+
+//TODO: finalize error handling
+// https://github.com/vercel/next.js/discussions/17832
+type NextController = (
+  req: NextApiRequest,
+  res: NextApiResponse<unknown>
+) => Promise<unknown>
+
+export default function catchErrorsFrom(controller: NextController) {
+  return async (
+    req: NextApiRequest,
+    res: NextApiResponse
+  ): Promise<unknown> => {
+    return controller(req, res).catch((error) => {
+      console.error(error)
+      return res.status(500).send(error.message || error)
+    })
+  }
+}
