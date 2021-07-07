a874cce7ed492bb1370b1d2ae8aa90c5fa0a59b7
vovaushenko <vovaushenko1989@gmail.com>
2021-07-07 18:43:27 +0300

    Add create new product enpoint and route controller

--- controllers/productsController.ts
+++ controllers/productsController.ts
@@ -3,9 +3,12 @@ import catchErrorsFrom from '../middleware/catchErrorsFrom';
 import Products from '../models/products';
 
 /**
- * Get all products
- * @function
+ * Get all products route controller
  * @GET /api/news
+ * @function
+ * @param {Next.Request} req  The Next request
+ * @param {Next.Response} res The Next response
+ * @return {undefined}
  */
 
 export const getAllProducts = catchErrorsFrom(
@@ -15,6 +18,26 @@ export const getAllProducts = catchErrorsFrom(
       success: true,
       count: allProducts.length,
       allProducts,
+    });
+  }
+);
+
+/**
+ * Create new product route controller
+ * @POST /api/news
+ * @function
+ * @param {Next.Request} req  The Next request
+ * @param {Next.Response} res The Next response
+ * @return {undefined}
+ */
+
+export const addNewProduct = catchErrorsFrom(
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    //TODO: @upload news images to Cloudinary CDN
+    const newProduct = await Products.create(req.body);
+    res.status(200).json({
+      success: true,
+      newProduct,
     });
   }
 );
--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,13 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
 import connectWithDB from '../../../config/connectWithDB';
-import { getAllProducts } from '../../../controllers/productsController';
+import {
+  addNewProduct,
+  getAllProducts,
+} from '../../../controllers/productsController';
 import { onError } from '../../../middleware/onError';
 
 const handler = nc<NextApiRequest, NextApiResponse>({ onError });
 
 connectWithDB();
 
-handler.get(getAllProducts);
+handler.get(getAllProducts).post(addNewProduct);
 
 export default handler;
