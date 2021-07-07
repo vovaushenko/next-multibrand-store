de1cc42f2606e432aae24c8acc6904d9d2c36196
vovaushenko <vovaushenko1989@gmail.com>
2021-07-07 16:37:24 +0300

    Implement catch-all error handling middleware for Mongoose validation Errors

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -2,8 +2,9 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
 import connectWithDB from '../../../config/connectWithDB';
 import { getAllProducts } from '../../../controllers/productsController';
+import { onError } from '../../../middleware/onError';
 
-const handler = nc<NextApiRequest, NextApiResponse>();
+const handler = nc<NextApiRequest, NextApiResponse>({ onError });
 
 connectWithDB();
 
