483826bd0976ca4c43a77b0f19795dccb8ed0b94
vovaushenko <vovaushenko1989@gmail.com>
2021-07-26 19:44:08 +0300

    refactor(dashboard-card): Start refactoring to cater different card types

--- components/DashboardCard/DashboardCard.tsx
+++ components/DashboardCard/DashboardCard.tsx
@@ -1,25 +1,75 @@
+import Image from 'next/image';
 import React, { ReactNode } from 'react';
 import * as Styled from './styles.DashboardCard';
 
 export interface Props {
-  variant: 'icon' | 'figure';
-  icon: ReactNode;
+  variant: 'icon' | 'figure' | 'stats';
+  title?: string;
+  imgSrc?: string;
+  icon: ReactNode | null;
   header: string;
   stat: string;
+  stats?: [string, string][];
 }
 /**
  *@function DashboardCard
 
  *@returns {JSX.Element} - Rendered DashboardCard component
  */
-const DashboardCard = ({ icon, header, stat }: Props): JSX.Element => {
+const DashboardCard = ({
+  icon,
+  header,
+  stat,
+  variant,
+  stats,
+  title,
+  imgSrc,
+}: Props): JSX.Element => {
+  if (variant === 'icon') {
+    return (
+      <Styled.Container>
+        <Styled.TextWrapper>
+          <Styled.Header>{header}</Styled.Header>
+          <Styled.Stat>{stat}</Styled.Stat>
+        </Styled.TextWrapper>
+        <Styled.ImageWrapper>{icon}</Styled.ImageWrapper>
+      </Styled.Container>
+    );
+  }
+
+  if (variant === 'figure') {
+    return (
+      <Styled.Container>
+        <Styled.Title>{title && title}</Styled.Title>
+        <Styled.TextContainer>
+          {stats?.map((stat, id) => (
+            <Styled.TextWrapper key={id}>
+              <Styled.Header>{stat[0]}</Styled.Header>
+              <Styled.Stat>{stat[1]}</Styled.Stat>
+            </Styled.TextWrapper>
+          ))}
+        </Styled.TextContainer>
+        <Styled.Figure>
+          <Image
+            src={imgSrc || ''}
+            alt={title}
+            height={200}
+            width={300}
+            objectFit="contain"
+          />
+        </Styled.Figure>
+      </Styled.Container>
+    );
+  }
+
   return (
     <Styled.Container>
-      <Styled.TextWrapper>
-        <Styled.Header>{header}</Styled.Header>
-        <Styled.Stat>{stat}</Styled.Stat>
-      </Styled.TextWrapper>
-      <Styled.ImageWrapper>{icon}</Styled.ImageWrapper>
+      {stats?.map((stat, id) => (
+        <Styled.TextWrapper key={id}>
+          <Styled.Header>{stat[0]}</Styled.Header>
+          <Styled.Stat>{stat[1]}</Styled.Stat>
+        </Styled.TextWrapper>
+      ))}
     </Styled.Container>
   );
 };
--- components/DashboardCard/styles.DashboardCard.tsx
+++ components/DashboardCard/styles.DashboardCard.tsx
@@ -5,17 +5,42 @@ export const Container = styled.article`
   align-items: center;
   justify-content: space-between;
   flex-wrap: wrap;
-  padding: 1rem;
+  padding: 2rem 1rem;
   border-radius: ${({ theme }) => theme.borderRadius};
   background-color: ${({ theme }) => theme.dashboardGrayCard};
+  width: 100%;
+
+  &:hover {
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.secondaryGolden};
+  }
 `;
 
-export const TextWrapper = styled.div``;
+export const TextWrapper = styled.div`
+  text-align: center;
+`;
 
-export const Header = styled.h3`
+export const TextContainer = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  gap: 4rem;
+`;
+
+export const Title = styled.h3`
+  font-size: 1.5rem;
+  font-weight: 400;
+  color: ${({ theme }) => theme.primaryGolden};
+  text-transform: capitalize;
+  margin-bottom: 1rem;
+  text-align: center;
+`;
+
+export const Header = styled.h4`
   font-size: 1rem;
   font-weight: 400;
   color: ${({ theme }) => theme.lightGray};
+  text-transform: capitalize;
 `;
 export const Stat = styled.p`
   font-size: 1.5rem;
@@ -28,11 +53,14 @@ export const ImageWrapper = styled.figure`
   align-items: center;
   justify-content: center;
   border-radius: 50%;
-  background-color: ${({ theme }) => theme.primaryGolden};
-  padding: 0.5rem;
+  background-color: ${({ theme }) => theme.dashboardGrayBg};
+  padding: 0.75rem;
   margin-left: 1rem;
 
   .icon {
     font-size: 2rem;
+    color: ${({ theme }) => theme.primaryGolden};
   }
 `;
+
+export const Figure = styled.figure``;
