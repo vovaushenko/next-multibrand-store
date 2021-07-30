ee1339a3c262c187b113ea336167ddd4e8ef2324
vovaushenko <vovaushenko1989@gmail.com>
2021-07-30 14:02:42 +0300

    refactor: update SettingsControl component

--- components/SettingsControl/SettingsControl.tsx
+++ components/SettingsControl/SettingsControl.tsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import Link from 'next/link';
+import React, { useCallback, useState } from 'react';
 import { BiCog } from 'react-icons/bi';
+import {
+  MdAccountCircle,
+  MdBrightness6,
+  MdSms,
+  MdWbSunny,
+} from 'react-icons/md';
 import * as Styled from './styles.SettingsControl';
 
+/**
+ *Settings control element
+ *@allow customers to change theme, contact support
+ *@function SettingsControl
+ *@returns {JSX.Element} - Rendered SettingsControl component
+ */
+
 const SettingsControl = (): JSX.Element => {
+  const [isSettingsShown, setIsSettingsShown] = useState(false);
+
+  const handleShowSettings = useCallback(() => {
+    setIsSettingsShown((prev) => !prev);
+  }, []);
+
   return (
-    <Styled.Container>
-      <BiCog className="icon" />
-    </Styled.Container>
+    <>
+      <Styled.Container onClick={handleShowSettings} title="Settings">
+        <BiCog className="icon" />
+      </Styled.Container>
+      {isSettingsShown && (
+        <Styled.SettingsContainer>
+          <Styled.SettingsOption title="Change Theme">
+            <MdBrightness6 className="icon" />
+          </Styled.SettingsOption>
+          <Styled.SettingsOption>
+            <MdWbSunny className="icon" />
+          </Styled.SettingsOption>
+          <Styled.SettingsOption title="Contact us">
+            <MdSms className="icon" />
+          </Styled.SettingsOption>
+          <Styled.SettingsOption title="Account">
+            <Link href="/account" passHref>
+              <MdAccountCircle className="icon" />
+            </Link>
+          </Styled.SettingsOption>
+        </Styled.SettingsContainer>
+      )}
+    </>
   );
 };
 
--- components/SettingsControl/styles.SettingsControl.tsx
+++ components/SettingsControl/styles.SettingsControl.tsx
@@ -1,30 +1,77 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.aside`
+const sharedContainerStyles = css`
+  /* Center Icon */
   display: flex;
   align-items: center;
   justify-content: center;
+  /* Styles */
   max-width: 4rem;
   padding: 0.5rem;
   background-image: ${({ theme }) => theme.blackToGrayGradient};
   border-radius: 2rem;
   border: 2px solid ${({ theme }) => theme.secondaryGolden};
   cursor: pointer;
-  margin-top: 15rem;
+`;
 
+export const Container = styled.aside`
+  /* Positioning */
   position: fixed;
-  top: 50%;
+  top: 85%;
   z-index: 6;
   left: calc(100% - 4rem);
+  /* Shared Styles */
+  ${sharedContainerStyles}
 
   .icon {
+    transition: all 3s cubic-bezier(0.075, 0.82, 0.165, 1);
     font-size: 2rem;
     color: ${({ theme }) => theme.primaryWhite};
-    transition: all 2s cubic-bezier(0.075, 0.82, 0.165, 1);
   }
   &:hover {
     .icon {
       transform: rotate(360deg);
     }
   }
+`;
+
+export const SettingsContainer = styled.nav`
+  /* positioning */
+  position: fixed;
+  top: 66%;
+  z-index: 7;
+  left: calc(100% - 9rem);
+  /* display */
+  display: flex;
+  flex-direction: column;
+  transform: rotateZ(20deg);
+
+  gap: 0.3rem;
+  padding: 1rem;
+`;
+
+export const SettingsOption = styled.div`
+  /* shared styles */
+  ${sharedContainerStyles}
+  /* rotation and shape */
+
+  
+  .icon {
+    transform: rotateZ(-20deg);
+    transition: all 3s cubic-bezier(0.075, 0.82, 0.165, 1);
+    font-size: 2rem;
+    color: ${({ theme }) => theme.primaryWhite};
+  }
+
+  @keyframes scale-in-ver-center {
+    0% {
+      transform: scale(0);
+      opacity: 1;
+    }
+    100% {
+      transform: scale(1);
+      opacity: 1;
+    }
+  }
+  animation: scale-in-ver-center 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
 `;
